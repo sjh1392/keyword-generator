b202aa61ef5ad9560ae55cd5e44603956ad2e8c3
sjhfmuk <50488800+sjhfmuk@users.noreply.github.com>
2025-01-26 08:06:54 +0000

    moved logging to main

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ import OpenAIApi from "openai";
 import axios from "axios";
 import cors from "cors";
 
-
 import log from "./service/logService.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const _projectId = 'impactio-439213';
+
+const { Logging } = require('@google-cloud/logging');
 
 app.use(cors());
 
@@ -67,6 +68,56 @@ app.get('/api/keyword-volume', async (req, res) => {
             console.log(error)
         })
 });
+
+
+app.get('/api/log', async(req,res)=> { 
+
+    const {data} = req.query;
+    const {name} = req.query || '';
+    const {session} = req.query;
+ 
+    if (!data) {
+        console.log(req.query);
+        return res.status(499).json({ error: 'log object is required' });
+    }
+
+    try {
+        const logResult = await testLog(name, data, session);
+        res.json({ logResult });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while sending log' });
+    }
+
+});
+
+
+async function testLog(name, data, session) {
+  const logging = new Logging({ projectId: _projectId });
+  const logName = name || 'test-log';
+  const log = logging.log(logName);
+
+  const metadata = {
+    resource: { type: 'global' },
+  };
+
+  const entry = log.entry(metadata, {
+    message: {data, session},
+    severity: 'INFO',
+  });
+
+  try {
+    await log.write(entry);
+    console.log('Log successfully written!');
+    return {'entry': entry, "status" : "success"};
+  } catch (err) {
+    console.error('Error writing log:', err);
+    return {'entry': entry, "status" :"fail", "message": err};
+  }
+}
+
+
+
 
 // Function to fetch related phrases using OpenAI API
 async function getRelatedPhrases(keyword) {
